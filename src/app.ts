b04e7d7e498c56c9b4621937ad09194c6a2ca26d
Wan Muhammad Danial <danial.seniwan@tmrnd.com.my>
2024-08-11 23:33:06 +0800

    custom redux-store

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,55 @@
-import { renderTodos } from './utils';
+import * as fromStore from "./store";
 
-const input = document.querySelector('input') as HTMLInputElement;
-const button = document.querySelector('button') as HTMLButtonElement;
-const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
-const todoList = document.querySelector('.todos') as HTMLLIElement;
+import { renderTodos } from "./utils";
+
+const input = document.querySelector("input") as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
+const todoList = document.querySelector(".todos") as HTMLLIElement;
+
+const reducers = {
+  todos: fromStore.reducer,
+};
+
+// const state = {
+//   todos: {
+//     loaded: false,
+//     loading: false,
+//     data: []
+//   }
+// }
+
+const store = new fromStore.Store(reducers, {});
 
 button.addEventListener(
-  'click',
+  "click",
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+    const todo = { label: input.value, complete: false };
 
-    console.log(payload);
+    // old way without action creator
+    // store.dispatch({ type: fromStore.ADD_TODO, payload });
 
-    input.value = '';
+    store.dispatch(new fromStore.AddTodo(todo));
+
+    input.value = "";
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+const unsubscribe = store.subscribe((state) => {
+  renderTodos(state.todos.data);
+});
+
+destroy.addEventListener("click", unsubscribe, false);
+
+todoList.addEventListener("click", function (event) {
   const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
-    console.log(target);
+  if (target.nodeName.toLowerCase() === "button") {
+    const todo = JSON.parse(target.getAttribute("data-todo") as any);
+    store.dispatch(new fromStore.RemoveTodo(todo));
   }
 });
+
+store.subscribe((state) => console.log("state:::", state));
---
+++ src/store/store.ts
@@ -0,0 +1,57 @@
+export class Store {
+  private subscribers: Function[];
+  private reducers: { [key: string]: Function };
+  private state: { [key: string]: any };
+
+  constructor(reducers = {}, initialState = {}) {
+    this.subscribers = [];
+    this.reducers = reducers;
+    this.state = this.reduce(initialState, {});
+  }
+
+  get value() {
+    return this.state;
+  }
+
+  subscribe(fn) {
+    this.subscribers = [...this.subscribers, fn];
+    this.notify();
+    return () => {
+      this.subscribers = this.subscribers.filter((sub) => sub !== fn);
+    };
+  }
+
+  dispatch(action) {
+    // this.state = {
+    //   /** it is copying the previous state data so if it has a todos, it will have a todo */
+    //   ...this.state,
+    //   /** after it is done copying that state, then we access it with todos */
+    //   todos: [...this.state.todos, action.payload],
+    // };
+    this.state = this.reduce(this.state, action);
+    this.notify();
+  }
+
+  private notify() {
+    /**loop through subscribers list and pass the state */
+    this.subscribers.forEach((fn) => fn(this.value));
+  }
+
+  private reduce(state, action) {
+    const newState = {};
+    /** prop is the key of the object from reducers */
+    for (const prop in this.reducers) {
+      /**this then becomes newState.todos  which is essentially
+       * newState.todos = this.reducers.todos()
+       *
+       * loop over all reducers
+       * create new object property for each one
+       * bind the value as the result of each reducer
+       * each reducer manages its own property reducer.todo(state.todo, action)
+       */
+
+      newState[prop] = this.reducers[prop](state[prop], action);
+    }
+    return newState;
+  }
+}
